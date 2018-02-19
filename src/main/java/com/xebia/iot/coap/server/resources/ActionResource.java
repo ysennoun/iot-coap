66b87138a7fb810a5d996500906e33fe2d43a207
sennoun <ysennoun@xebia.fr>
2018-02-19 20:34:55 +0100

    Reformat json in action

--- src/main/java/com/xebia/iot/coap/server/resources/ActionResource.java
+++ src/main/java/com/xebia/iot/coap/server/resources/ActionResource.java
@@ -3,11 +3,7 @@ package com.xebia.iot.coap.server.resources;
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.core.server.resources.CoapExchange;
-
-import java.io.IOException;
 import java.util.Random;
-
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 public class ActionResource extends CoapResource {
 
@@ -18,18 +14,9 @@ public class ActionResource extends CoapResource {
 
     @Override
     public void handleGET(CoapExchange exchange) {
-
         Random r = new Random();
         String payload = String.valueOf(r.nextInt(50));
-        try {
-            String jsonPayload = jsonBuilder()
-                    .startObject()
-                    .field("code", payload)
-                    .endObject()
-                    .toString();
-            exchange.respond(CoAP.ResponseCode.CONTENT, jsonPayload);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String jsonPayload =  "{\"code\":"+ payload + "}" ;
+        exchange.respond(CoAP.ResponseCode.CONTENT, jsonPayload);
     }
 }
